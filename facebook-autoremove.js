148921ce845d8c24452bcd1729c96bcc740eb94a
Ben Shapiro <bms@uoregon.edu>
2019-08-07 20:52:55 -0700

    completed functions

--- facebook-autoremove.js
+++ facebook-autoremove.js
@@ -6,27 +6,141 @@ var removeGroupMembers = (function () {
 	var scriptEnabled = false;
 	var processing = false;
 
-	removeGroupMembers.start = function() {};
+	removeGroupMembers.start = function() {
+		scriptEnabled = true;
+		removeAll();
+	};
 
-	removeGroupMembers.stop = function() {};
+	removeGroupMembers.stop = function() {
+		scriptEnabled = false;
+	};
 
-	function removeALL() {}
+	function removeAll() {
+		if (scriptEnabled) {
+			listMembersToRemove();
+			processList();
+		}
+	}
 
-	function listMembersToRemove() {}
+	function listMembersToRemove() {
+		var adminActions = document.getElementsByClassName('adminActions');
+		console.log(excludedFbIDs);
+		for(var i=0; i<adminActions.length; i++) {
+			var gearWheelIconDiv = adminActions[i];
+			var hyperlinksInAdminDialog = gearWheelIconDiv.getElementsByTagName('a');
+			var	var FbMemberName = getTextFromElement(gearWheelIconDiv.parentNode.parentNode.parentNode.getElementsByClassName('fcb')[0]);
+			if (excludedFbIds.indexOf(FbmemberID) != -1) {
+				console.log("SKIPPING "+FbMemberName+' ('+FbmemberID+')');
+				continue;
+			} else {
+				usersToRemoveQueue.push({'memberID': FbmemberID, 'gearWheelIconDiv': gearWheelIconDiv})
+			}
+		}
+	}
 
-	function processList() {}
+	function processList() {
+		if (!scriptEnabled) {
+            return;
+        }
+        if (usersToRemoveQueue.length > 0) {
+            removeNext();
+ 
+            setTimeout(function(){
+                processList();
+            },1000);
+        } else {
+            getMore();
+        }
+	}
 
-	function removeNext() {}
+	function removeNext() {
+		if (!scriptEnabled) {
+            return;
+        }
+        if (usersToRemoveQueue.length > 0) {
+            var nextElement = usersToRemoveQueue.pop();
+            removeMember(nextElement.memberID, nextElement.gearWheelIconDiv);
+        }
+	}
 
-	function removeMember() {}
+	function removeMember(memberID, gearWheelIconDiv) {
+		if (processing) {
+            return;
+        }
+        var gearWheelHref = gearWheelIconDiv.getElementsByTagName('a')[0];
+        gearWheelHref.click();
+        processing = true;
+        setTimeout(function(){
+            var popupRef = gearWheelHref.id;
+            var popupDiv = getElementByattributeibute('data-ownerid',popupRef);
+            var popupLinks = popupDiv.getElementsByTagName('a');
+            for(var j=0; j<popupLinks.length; j++) {
+                if (popupLinks[j].getattributeibute('href').indexOf('remove.php') !== -1) {
+                    // this is the remove link
+                    popupLinks[j].click();
+                    setTimeout(function(){
+                        var confirmButton = document.getElementsByClassName('layerConfirm uiOverlayButton selected')[0];
+                        var errorDialog = getElementByattributeibute('data-reactid','.4.0');
+                        if (confirmButton != null) {
+                            if (canClick(confirmButton)) {
+                                confirmButton.click();
+                            } else {
+                                console.log('This should not happen memberID: '+memberID);
+                                5/0;
+                                console.log(gearWheelIconDiv);
+                            }
+                        }
+                        if (errorDialog != null) {
+                            console.log("Error while removing member "+memberID);
+                            errorDialog.getElementsByClassName('selected  layerCancel autofocus')[0].click();
+                        } 
+                        processing = false;
+                    },700);
+                    continue;
+                }
+            }
+        },500);
+	}
 
-	function canClick() {}
+	function canClick(elem) {
+		return (typeof elem != 'undefined') && (typeof elem.click != 'undefined');
+	}
 
-	function getMore() {}
+	function getMore() {
+		processing = true;
+        more = document.getElementsByClassName("pam uiBoxLightblue  uiMorePagerPrimary");
+        if (typeof more != 'undefined' && canClick(more[0])) {
+            more[0].click();
+            setTimeout(function(){
+                removeAll();
+                processing = false;
+           }, 2000);
+        } else {
+            removeGroupMembers.stop();
+        }
+	}
 
-	function getTextFromElement() {}
+	function getTextFromElement(element) {
+		var text = element.textContent;
+        return text;
+	}
 
-	function getElementByAttribute() {}
+	function getElementByattributeibute(attribute, value, root) {
+		root = root || document.body;
+        if(root.hasattributeibute(attribute) && root.getattributeibute(attribute) == value) {
+            return root;
+        }
+        var children = root.children,
+            element;
+        for(var i = children.length; i--; ) {
+            element = getElementByattributeibute(attribute, value, children[i]);
+            if(element) {
+                return element;
+            }
+        }
+        return null;
+	}
 
+	return removeGroupMembers;
 })();
 removeGroupMembers.start();
