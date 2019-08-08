dca0e35ebe3f06dcad4cb15fb485d47f651f3b3d
Ben Shapiro <bms@uoregon.edu>
2019-08-07 20:29:54 -0700

    added functions and objects

--- facebook-autoremove.js
+++ facebook-autoremove.js
@@ -1,4 +1,32 @@
 var removeGroupMembers = (function () {
+	var removeGroupMembers = {};
+	// NEED ANNOTATION HERE
+	var excludedFbIDs = ['abcdefghijklm']; //NEED ANNOTATION HERE
+	var usersToRemoveQueue = [];
+	var scriptEnabled = false;
+	var processing = false;
+
+	removeGroupMembers.start = function() {};
+
+	removeGroupMembers.stop = function() {};
+
+	function removeALL() {}
+
+	function listMembersToRemove() {}
+
+	function processList() {}
+
+	function removeNext() {}
+
+	function removeMember() {}
+
+	function canClick() {}
+
+	function getMore() {}
+
+	function getTextFromElement() {}
+
+	function getElementByAttribute() {}
 
 })();
 removeGroupMembers.start();
